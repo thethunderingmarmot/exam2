7c472c9b1642ad9513bd13f317f6b94e66bb4ede
Tommaso De Tommaso <tommaso.detommaso@studio.unibo.it>
2023-12-15 17:12:04 +0100

    Tried to change solution to a more classic OOP

--- src/a04/sol1/BankAccountFactoryImpl.java
+++ src/a04/sol1/BankAccountFactoryImpl.java
@@ -6,75 +6,40 @@ import java.util.function.UnaryOperator;
 
 public class BankAccountFactoryImpl implements BankAccountFactory {
 
-    private static abstract class AbstractBankAccount implements BankAccount {
-        protected abstract void setBalance(int amount);
-    }
+    private class BasicBankAccount implements BankAccount {
 
-    private static class BankAccountDecorator extends AbstractBankAccount {
-        private final AbstractBankAccount base;
-        public BankAccountDecorator(AbstractBankAccount base){
-            this.base = base;
-        }
+        protected int balance;
+
+        @Override
         public int getBalance() {
-            return base.getBalance();
+            return balance;
         }
+
+        @Override
         public void deposit(int amount) {
-            base.deposit(amount);
+            balance += amount;
         }
+
+        @Override
         public boolean withdraw(int amount) {
-            return base.withdraw(amount);
-        }
-        
-        protected void setBalance(int amount) {
-            base.setBalance(amount);
+            if(balance > amount) {
+                balance -= amount;
+                return true;
+            }
+            return false;
         }
     }
 
     @Override
     public BankAccount createBasic() {
-        return refinedCreateBasic();
-    }
-
-    private AbstractBankAccount refinedCreateBasic() {
-        return new AbstractBankAccount() {
-
-            private int balance = 0;
-
-            @Override
-            public int getBalance() {
-                return this.balance;
-            }
-
-            @Override
-            protected void setBalance(int amount){
-                this.balance = amount;
-            }
-
-            @Override
-            public void deposit(int amount) {
-                this.balance = this.balance + amount;
-            }
-
-            @Override
-            public boolean withdraw(int amount) {
-                if (this.balance < amount){
-                    return false;
-                }
-                this.balance = this.balance - amount;
-                return true;
-            }
-        };
+        return new BasicBankAccount();
     }
 
     @Override
     public BankAccount createWithFee(UnaryOperator<Integer> feeFunction) {
-        return refinedCreateWithFee(feeFunction, refinedCreateBasic());
-    }
-
-    private AbstractBankAccount refinedCreateWithFee(UnaryOperator<Integer> feeFunction, AbstractBankAccount base) {
-        return new BankAccountDecorator(base){
+        return new BasicBankAccount() {
             @Override
-            public boolean withdraw(int amount){
+            public boolean withdraw(int amount) {
                 return super.withdraw(amount + feeFunction.apply(amount));
             }
         };
@@ -82,50 +47,49 @@ public class BankAccountFactoryImpl implements BankAccountFactory {
 
     @Override
     public BankAccount createWithCredit(Predicate<Integer> allowedCredit, UnaryOperator<Integer> rateFunction){
-        return refinedCreateWithCredit(allowedCredit, rateFunction);
-    }
+        return new BasicBankAccount() {
 
-    public AbstractBankAccount refinedCreateWithCredit(Predicate<Integer> allowedCredit, UnaryOperator<Integer> rateFunction) {
-        return new BankAccountDecorator(refinedCreateBasic()){
             @Override
-            public boolean withdraw(int amount){
-                if (super.withdraw(amount)){
-                    return true;
-                }
-                final var credit = amount - this.getBalance();
-                if (allowedCredit.test(credit)){
-                    setBalance(-credit-rateFunction.apply(-credit));
-                    return true;
-                }
-                return false;
+            public boolean withdraw(int amount) {
+                return allowedCredit.test((balance - amount) * -1)
+                     ? super.withdraw(amount + rateFunction.apply(amount))
+                     : false;                
             }
+
         };
     }
 
     @Override
     public BankAccount createWithBlock(BiPredicate<Integer, Integer> blockingPolicy) {
-        return new BankAccountDecorator(refinedCreateBasic()){
-            private boolean blocked = false;
+        return new BasicBankAccount() {
 
+            
             @Override
-            public boolean withdraw(int amount){
-                if (blocked) {
-                    return false;
-                }
-                if (blockingPolicy.test(amount, super.getBalance())){
-                    blocked = true;
-                    return false;
-                }
-                return super.withdraw(amount);
+            public boolean withdraw(int amount) {
+                // TODO Auto-generated method stub
+                throw new UnsupportedOperationException("Unimplemented method 'withdraw'");
             }
+
         };
     }
 
     @Override
     public BankAccount createWithFeeAndCredit(UnaryOperator<Integer> feeFunction, Predicate<Integer> allowedCredit,
             UnaryOperator<Integer> rateFunction) {
-       return refinedCreateWithFee(feeFunction, refinedCreateWithCredit(allowedCredit, rateFunction));
-    }
+        return new BasicBankAccount() {
 
-   
+            @Override
+            public void deposit(int amount) {
+                // TODO Auto-generated method stub
+                throw new UnsupportedOperationException("Unimplemented method 'deposit'");
+            }
+
+            @Override
+            public boolean withdraw(int amount) {
+                // TODO Auto-generated method stub
+                throw new UnsupportedOperationException("Unimplemented method 'withdraw'");
+            }
+
+        };
+    }
 }
